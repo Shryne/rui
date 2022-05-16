0b133327b07152bfd0b311d4a108bec6cc181b4d
Shryne <enomeral@gmx.de>
2022-05-16 21:28:35 +0200

    implement Pos and Size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fmt::{Display, Formatter};
+
+struct Pos(i32, i32);
+struct Size(i32, i32);
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", Pos(0, 0));
+}
+
+impl Pos {
+    fn x(&self) -> i32 { self.0 }
+    fn y(&self) -> i32 { self.1 }
+}
+
+impl Display for Pos {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Pos({}, {})", self.x(), self.y())
+    }
+}
+
+impl Size {
+    fn w(&self) -> i32 { self.0 }
+    fn h(&self) -> i32 { self.1 }
+}
+
+impl Display for Size {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Size({}, {})", self.w(), self.h())
+    }
 }
